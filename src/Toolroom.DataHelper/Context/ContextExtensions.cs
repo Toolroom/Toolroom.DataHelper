53a1f895e4da5c575f43254fb4f5d6127827cd26
Peter Malli-Trinkl <p.malli@bitbird.at>
2019-11-21 17:32:34 +0100

    calling DetectChanges less often

--- src/Toolroom.DataHelper/Context/ContextExtensions.cs
+++ src/Toolroom.DataHelper/Context/ContextExtensions.cs
@@ -8,25 +8,44 @@ namespace Toolroom.DataHelper
         public static void HandleSaveChanges(this DbContext context, int? editUserId)
         {
             context.ChangeTracker.DetectChanges();
-            var changedItems = context.ChangeTracker.Entries();
-            foreach (var item in changedItems)
+            bool detectAgain = false;
             {
-                if (item.Entity is IXmlEntity xmlEntity)
+                var changedItems = context.ChangeTracker.Entries();
+                foreach (var item in changedItems)
                 {
-                    xmlEntity.XmlValues = xmlEntity.GetXmlValuesFromProperties(typeof(XmlMappedAttribute));
-                    context.ChangeTracker.DetectChanges();
-                }
-                if (item.State == EntityState.Added || item.State == EntityState.Modified)
-                {
-                    //store edit user id automatically
-                    if (item.Entity is IEditUserEntity editUserEntity)
-                        editUserEntity.EditUserId = editUserId;
-
-                    //compute entities
-                    (item.Entity as IComputationEntity)?.Compute();
+                    if (item.Entity is IXmlEntity xmlEntity)
+                    {
+                        xmlEntity.XmlValues = xmlEntity.GetXmlValuesFromProperties(typeof(XmlMappedAttribute));
+                        
+                        detectAgain = true;
+                    }
                 }
             }
-            context.ChangeTracker.DetectChanges();
+
+            if (detectAgain)
+                context.ChangeTracker.DetectChanges();
+            detectAgain = false;
+
+            {
+                var changedItems = context.ChangeTracker.Entries();
+                foreach (var item in changedItems)
+                {
+                    if (item.State == EntityState.Added || item.State == EntityState.Modified)
+                    {
+                        //store edit user id automatically
+                        if (item.Entity is IEditUserEntity editUserEntity)
+                            editUserEntity.EditUserId = editUserId;
+
+                        //compute entities
+                        (item.Entity as IComputationEntity)?.Compute();
+                        
+                        detectAgain = true;
+                    }
+                }
+            }
+
+            if (detectAgain)
+                context.ChangeTracker.DetectChanges();
         }
 
         public static void ObjectMaterialized(object sender, ObjectMaterializedEventArgs e)
